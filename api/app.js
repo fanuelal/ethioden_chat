2f54634ee43513a109e9a20f7739bdd9cdc2a600
ademecheklie <ademecheklie78@gmail.com>
2023-05-18 11:01:35 +0300

    kk

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 
 app.post('/api/v1/auth', authenticate)
-app.use('/api/v1/', authorize, apiRoutes)
+app.use('/api/v1/', apiRoutes)
 // sequelize.sync()
 const PORT = process.env.PORT
 
