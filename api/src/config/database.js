4a8fc600262d856647a90c3e5f6a2ec6cfaf86fa
ademecheklie <ademecheklie78@gmail.com>
2023-05-18 10:59:15 +0300

    added axiox instance

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,12 +1,14 @@
-import  mysql  from "mysql";
+import  mysql  from "mysql2";
 import dotenv from 'dotenv';
-
 dotenv.config()
+
+
 const con =  mysql.createConnection({
     // host: "localhost",
     // user: "root",
     // password: "",
     // database: "ethioden"
+    
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
