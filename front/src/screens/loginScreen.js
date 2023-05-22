1ed58b582e734a3ca50d7db0ad7f895671e75063
ademecheklie <ademecheklie78@gmail.com>
2023-05-22 10:51:26 +0300

    auth config and login modified

--- front/src/screens/loginScreen.js
+++ front/src/screens/loginScreen.js
@@ -1,20 +1,47 @@
 import React, { useState } from 'react';
 import { TextField, Button } from '@mui/material';
-import { EmailOutlined, LockOutlined, Visibility, VisibilityOff  } from '@mui/icons-material';
+import { EmailOutlined, LockOutlined, Visibility, VisibilityOff } from '@mui/icons-material';
 import IconButton from '@mui/material/IconButton';
 import { useNavigate } from 'react-router-dom';
 import '../styles/login.css';
+import axiosInstance from '../config/axiosConfig';
+import { setToken, getToken } from '../config/tokenManager';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false)
-  const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginError, setLoginError] = useState(false);
 
-  const submitHandler = (e) => {
-    
-    navigate('/home')
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post('/auth', { email, password });
+  
+      if (response.data.success) {
+        const token = response.data.data.genToken;
+        setToken(token);
+        console.log(getToken());
+  
+        navigate('/home');
+      } else {
+        setLoginError(true);
+  
+      
+      }
+    } catch (err) {
+      console.log(err);
+      setLoginError(true);
+  
+     
+      setTimeout(() => {
+        setLoginError(false);
+      }, 2000);
+    }
   };
+  
 
   const showPasswordHandler = () => setShowPassword((show) => !show);
 
@@ -29,37 +56,38 @@ function Login() {
           type="email"
           label="Email"
           variant="outlined"
-          className='login-input'
+          className="login-input"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           InputProps={{
-            startAdornment: <EmailOutlined className='icon' />,
+            startAdornment: <EmailOutlined className="icon" />,
           }}
         />
         <label htmlFor="password" className="login-label">
           Password
         </label>
         <TextField
-          type={showPassword ? "text": "password" }
+          type={showPassword ? 'text' : 'password'}
           label="Password"
           variant="outlined"
           className="login-input"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           InputProps={{
-            startAdornment: <LockOutlined className={"icon"} />,
-            endAdornment:                 <IconButton
-            aria-label="toggle password visibility"
-            onClick={showPasswordHandler}
-            edge="end"
-          >
-            {showPassword ? <VisibilityOff /> : <Visibility />}
-          </IconButton>
+            startAdornment: <LockOutlined className="icon" />,
+            endAdornment: (
+              <IconButton aria-label="toggle password visibility" onClick={showPasswordHandler} edge="end">
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            ),
           }}
         />
         <button type="submit" className="login-button">
           Login
         </button>
+        {loginError && (
+          <p className="login-error">Incorrect email or password. Please try again.</p>
+        )}
       </form>
     </div>
   );
