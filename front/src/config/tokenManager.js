3c76aa481a7b6681d4fb7699b2955f10e1fc8094
ademecheklie <ademecheklie78@gmail.com>
2023-05-22 21:58:50 +0300

    stroring token in local storage done

--- front/src/config/tokenManager.js
+++ front/src/config/tokenManager.js
@@ -2,8 +2,12 @@ let token = '';
 
 export const setToken = (newToken) => {
   token = newToken;
+  localStorage.setItem('token',token)
 };
 
 export const getToken = () => {
+  if(token !==null){
+    token = localStorage.getItem('token')
+  }
   return token;
 };
