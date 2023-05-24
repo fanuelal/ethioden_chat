23a0c39d6c181c735e49619c6ed805d4488a56d5
ademecheklie <ademecheklie78@gmail.com>
2023-05-24 12:00:17 +0300

    access restrict

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import './styles/App.css';
 import {ChatList} from './screens/recentChatContainer'
 import { ActiveData } from './controller/activeChatData';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { messages } from './model/message';
 import {EmptyScreen} from './screens/emptyChat'
 import Login from './screens/loginScreen';
@@ -9,11 +9,20 @@ import {Route, Routes, Navigate} from 'react-router-dom'
 import {CatagoryList} from './screens/catagoryList'
 import axiosInstance from './config/axiosConfig'
 import PrivateRoutes from './components/privateRoutes';
+import { getToken } from './config/tokenManager';
 function App() {
 
   const [selected, setSelected] = useState(-1);
   const [messagesData, setMessageData] = useState([])
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+const isLogin = () => {
+  const token =getToken()
+  if (!token) {
+    return false;
+  }
+  return true;
+};
 
   function chatSelectHandler(userId) {
     axiosInstance.get(`/chat?userId=${userId}`).then((value)=>{
@@ -32,14 +41,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Login setIsLoggedIn= {setIsLoggedIn}/>} />
+        <Route path='/' element={<Login />} />
         <Route path="/*" element={<Navigate to="/" />} />
-        <Route  element ={<PrivateRoutes isLoggedIn= {isLoggedIn} />}>
+        <Route  element ={<PrivateRoutes isLogin= {isLogin} />}>
             <Route path='/home' element={<Home onChatClick={chatSelectHandler} selected={selected} messagesData={messagesData}/>}  />
         </Route>
           
             </Routes>
-console.log(isLoggedIn)
+
       
     </div>
   );
