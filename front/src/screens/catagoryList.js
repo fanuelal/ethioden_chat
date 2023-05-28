6cba21fe9fcab00d6bf68c4bf9092116e057055a
ademecheklie <ademecheklie78@gmail.com>
2023-05-28 11:23:47 +0300

    logout Added

--- front/src/screens/catagoryList.js
+++ front/src/screens/catagoryList.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import {currentUser} from '../model/currentUserData'
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCommentDots, faUsers, faBullhorn, faCog, faInfoCircle, faQuestionCircle, faRobot } from '@fortawesome/free-solid-svg-icons';
-
-
+import { faCommentDots, faUsers, faBullhorn, faCog, faInfoCircle, faQuestionCircle, faRobot, faSignOut } from '@fortawesome/free-solid-svg-icons';
 import '../styles/chatList.css'
+
 export function CatagoryList() {
+ 
+    const navigate = useNavigate();
+    
+    const logoutHandler =()=>{
+        localStorage.removeItem('token');
+        navigate('/')
+    } 
     return(
       <div className="catagoryNav">
 
         <img className="chatProfile" alt='profileImage' src={currentUser.profileImg} />
-        <h3>{currentUser.username}</h3>
+        <h3 >{currentUser.username} </h3>
+        
         <ul className="vertical-nav">
       <li className="active">
 
@@ -53,6 +61,10 @@ export function CatagoryList() {
           <FontAwesomeIcon icon={faQuestionCircle} />
           <p>Help</p>
         
+      </li>
+      <li>
+      <FontAwesomeIcon icon={faSignOut} onClick={logoutHandler} />
+      <p>logout</p>
       </li>
     </ul>
       </div>  
