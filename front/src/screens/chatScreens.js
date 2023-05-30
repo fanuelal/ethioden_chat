e2c5b3836e0873a71a6804d45ad703971fa750e1
hermelalemma <asamenewhermela@gmail.com>
2023-05-31 00:09:55 +0300

    merging with kidist

--- front/src/screens/chatScreens.js
+++ front/src/screens/chatScreens.js
@@ -9,6 +9,7 @@ import { currentUser } from '../model/currentUserData'
 import { v4 as uuid } from 'uuid';
 import axiosConfig from '../config/axiosConfig'
 import Suggestionbox from '../components/suggestionbox'
+import { PopUp } from './PopUp'
 export function ChatUI(props){
 
 
@@ -89,8 +90,13 @@ export function ChatUI(props){
         <div className='ChatRoom'>
         <div className='profileNav'>
             {props.user.profileImg ? <img alt='user profile' className='chatProfile' src={props.user.profileImg} />: <img alt='user profile' className='chatProfile' src="https://thumbs.dreamstime.com/b/icon-profile-color-red-not-shadow-icon-profile-color-red-circle-color-dark-red-background-color-white-194702104.jpg" />}
-            
-            <h2>{props.username}</h2>
+            <h2>{props.user.username }</h2>
+            <div class="recentSentAt1">lastseen recently</div>
+            <div className='setstatus'>
+                <PopUp/>
+                
+
+            </div>
             </div> 
         <ChatListContainer messages={messages.length === 0? props.messages : messages} />
         <div> 
--- front/src/screens/recentChatContainer.js
+++ front/src/screens/recentChatContainer.js
@@ -27,8 +27,7 @@ export function ChatList(props){
         },[]) 
     // }
 
- const ListRecent = userList.map((user) => {
-     
+ const ListRecent = userList.map((user) => {  
         if(user.id !== currentUser.userId){
             return  <RecentChat onClick={recentClickHandler} userId={user.id} profileImg={"https://thumbs.dreamstime.com/b/icon-profile-color-red-not-shadow-icon-profile-color-red-circle-color-dark-red-background-color-white-194702104.jpg"} recentChat={"hello there"} status={true} username={user.first_name} />
         }
