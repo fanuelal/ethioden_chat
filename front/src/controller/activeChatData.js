7a09856d851eb4edd8e57e214594e8140f4797eb
kasiopiaTech <kasiopiatech@gmail.com>
2023-06-02 10:31:02 +0300

    merging fix

--- front/src/controller/activeChatData.js
+++ front/src/controller/activeChatData.js
@@ -4,9 +4,11 @@ import {ChatUI} from '../screens/chatScreens'
 
 export function ActiveData (props) {
 
-    console.log(props.username);
-    return (
-         <ChatUI user={props.userId} username={props.username} messages={props.messages}/>
-    );
+    // console.log(props.username);
+    if(props.username !== undefined){
+        return (
+             <ChatUI user={props.userId} username={props.username} messages={props.messages}/>
+        );
+    }
 }
 
