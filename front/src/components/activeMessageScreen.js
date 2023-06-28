fa4b7e4bdc8ec178f690fc4fb87a3959a913d968
kidist1234 <kidistmamaye0202@gmail.com>
2023-06-28 20:27:12 +0300

    edit and delet fix

--- front/src/components/activeMessageScreen.js
+++ front/src/components/activeMessageScreen.js
@@ -1,33 +1,33 @@
-import { React, useRef, useEffect, useState } from 'react';
-import '../styles/chatList.css';
-import { MessageView } from './singleChatMessage';
-import { currentUser } from '../model/currentUserData';
-import {formatMessageDate} from '../common/Common'
+import { React, useRef, useEffect, useState } from "react";
+import "../styles/chatList.css";
+import { MessageView } from "./singleChatMessage";
+import { currentUser } from "../model/currentUserData";
+import { formatMessageDate } from "../common/Common";
 
-export function ChatListContainer({ messages,onEdit }) {
+export function ChatListContainer({ messages, onEdit, onDelete}) {
   const messageDisplayRef = useRef(null);
   const [message, setMessage] = useState([]);
 
   const scrollToBottom = () => {
-    messageDisplayRef.current.scrollTop = messageDisplayRef.current.scrollHeight;
+    messageDisplayRef.current.scrollTop =
+      messageDisplayRef.current.scrollHeight;
   };
-  
+
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
- 
- 
   const handleEditClick = (messageID, message) => {
-   
     onEdit(messageID, message);
-   
+  };
+  const hendleDeleteClick =(messageID) => {
+    onDelete(messageID);
   };
 
   return (
-    <div className='MessageDisplay' ref={messageDisplayRef}>
+    <div className="MessageDisplay" ref={messageDisplayRef}>
       {messages.length === 0 ? (
-        <div className='emptyError'></div>
+        <div className="emptyError"></div>
       ) : (
         messages.map((message, index) => (
           <MessageView
@@ -36,8 +36,8 @@ export function ChatListContainer({ messages,onEdit }) {
             messageID={message.id}
             message={message.text}
             isSenders={currentUser.userId === message.senderId}
+            onDelete={hendleDeleteClick}
             onEdit={handleEditClick}
-            
           />
         ))
       )}
--- front/src/screens/chatScreens.js
+++ front/src/screens/chatScreens.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import '../styles/chatList.css'
-import '../components/chatTextField'
-import '../components/activeMessageScreen'
-import ChatSend from '../components/chatTextField'
-import {ChatListContainer}  from '../components/activeMessageScreen'
-import { useState, useEffect } from 'react'
-import { currentUser } from '../model/currentUserData'
-import { v4 as uuid } from 'uuid';
-import axiosConfig from '../config/axiosConfig'
-import { StatusPopUp } from './StatusPopUp'
+import React from "react";
+import "../styles/chatList.css";
+import "../components/chatTextField";
+import "../components/activeMessageScreen";
+import ChatSend from "../components/chatTextField";
+import { ChatListContainer } from "../components/activeMessageScreen";
+import { useState, useEffect } from "react";
+import { currentUser } from "../model/currentUserData";
+import { v4 as uuid } from "uuid";
+import axiosConfig from "../config/axiosConfig";
+import { StatusPopUp } from "./StatusPopUp";
 // import Suggestionbox from '../components/suggestionbox'
-export function ChatUI(props){
-    // console.log(props.copiedtext)
-    
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [channel, setChannel] = useState(null);
-    const [editedMessage, setEditedMessage] = useState(null);
-    // const channel = props.ably.channels.get('private_chat');
-    useEffect(()=>{
-        setMessages([])
-    }, [props.user])
+export function ChatUI(props) {
+  // console.log(props.copiedtext)
+  const [deleted, setDeleted ] = useState(null)
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [channel, setChannel] = useState(null);
+  const [editedMessage, setEditedMessage] = useState(null);
+  // const channel = props.ably.channels.get('private_chat');
+  useEffect(() => {
+    setMessages([]);
+  }, [props.user]);
 
   useEffect(() => {
     const ids = [currentUser.userId, props.user];
     const sortedId = ids.sort();
 
-    const newChannel = props.ably.channels.get(`private_chat:${sortedId[0]}${sortedId[1]}`);
+    const newChannel = props.ably.channels.get(
+      `private_chat:${sortedId[0]}${sortedId[1]}`
+    );
     setChannel(newChannel);
 
     newChannel.subscribe((message) => {
@@ -41,110 +43,140 @@ export function ChatUI(props){
       }
     };
   }, [props.user, props.ably.channels, channel]);
-        const onMessageAdd = (message) => {
-          const messageUUID = uuid();
-      
-          const newMessage = {
-            messageId: messageUUID,
-            text: message,
-            reciverId: props.user,
-            senderId: currentUser.userId,
-            created_at: new Date(),
-          };
-      
-          axiosConfig
-            .post('/chat/', {
-              text: message,
-              inRoom: 0,
-              roomId: null,
-              reciverId: props.user,
-              senderId: currentUser.userId,
-            })
-            .then((response) => {
-              console.log(response.data);
-             setMessages([...props.messages, newMessage]);
-            })
-            .catch((error) => {
-              throw error;
-            });
-        };
-    const onUpdateMessage = (messageID, updatedText) => {
-      const body = {
-        text: updatedText,
-      };
-  
-      axiosConfig
-        .patch(`/chat/${messageID}`, body)
-        .then((response) => {
-          console.log('Chat updated successfully:', response.data);
-          const updatedMessages = messages.map((msg) => {
-            if (msg.messageId === messageID) {
-              return { ...msg, text: updatedText };
-            }
-            return msg;
-          });
-          setMessages(updatedMessages);
-        })
-        .catch((error) => {
-          console.log('Error updating chat:', error);
+  const onMessageAdd = (message) => {
+    const messageUUID = uuid();
+
+    const newMessage = {
+      messageId: messageUUID,
+      text: message,
+      reciverId: props.user,
+      senderId: currentUser.userId,
+      created_at: new Date(),
+    };
+
+    axiosConfig
+      .post("/chat/", {
+        text: message,
+        inRoom: 0,
+        roomId: null,
+        reciverId: props.user,
+        senderId: currentUser.userId,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setMessages([...props.messages, newMessage]);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+  const onUpdateMessage = (messageID, updatedText) => {
+    const body = {
+      text: updatedText,
+    };
+
+    axiosConfig
+      .patch(`/chat/${messageID}`, body)
+      .then((response) => {
+        const updatedMessages = props.messages.map((msg) => {
+          console.log(msg.messageId === messageID);
+          console.log(messageID);
+          console.log(msg.id);
+          if (msg.id === messageID) {
+            return { ...msg, text: updatedText };
+          }
+          return msg;
         });
-    };
-
-    const onMessageSend = () => {
-      if (message.trim() !== '') {
-        if (editedMessage) {
-          onUpdateMessage(editedMessage.messageID, message);
-          setEditedMessage(null);
-        } else {
-          onMessageAdd(message);
-        }
-  
-        setMessage('');
+        console.log(updatedMessages);
+        setMessages(updatedMessages);
+      })
+      .catch((error) => {
+        console.log("Error updating chat:", error);
+      });
+  };
+  const onMessageSend = () => {
+    if (message.trim() !== "") {
+      if (editedMessage) {
+        onUpdateMessage(editedMessage.messageID, message);
+        setEditedMessage(null);
+      } else {
+        onMessageAdd(message);
       }
-    };
 
-    const handleInputChange = (event) => {
-        setMessage(event.target.value);
+      setMessage("");
     }
-    const onkeyPressHandler = (event) => {
-        if(event.key === 'Enter'){
-            event.preventDefault();
-            onMessageSend();
-        }
+  };
+
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+  };
+  const onkeyPressHandler = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      onMessageSend();
     }
-    const handleEdit = (messageID, message) => {
-      setEditedMessage({ messageID, message });
-      setMessage(message)
-    };
+  };
+  const handleEdit = (messageID, message) => {
+    setEditedMessage({ messageID, message });
+    setMessage(message);
+  };
+  const handleDelete = (messageID) => {
+    axiosConfig.delete(`/chat/${messageID}`)
+      .then((response) => {
+        const notDeletedMessages = props.messages.filter((msg) => msg.id !== messageID);
+        setMessages(notDeletedMessages);
+      })
+      .catch((error) => {
+        console.log("Error deleting message:", error);
+      });
+  }
   
-
-    return(
-        <div className='ChatRoom'   onContextMenu={(e) => {
-            e.preventDefault(); 
-            console.log("Right Click");
-            // console.log(props.isedited)
-          }}>
-        <div className='profileNav'>
-            {props.user.profileImg ? <img alt='user profile' className='chatProfile' src={props.user.profileImg} />: <img alt='user profile' className='chatProfile' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBrq9rrEZy6VUsQmoeIPh6gYzS_2JqKe1i9A&usqp=CAU" />}
-            <p className='profilename'>{props.username }</p>
-            <div class="recentSentAt1">lastseen recently</div>
-            <div className='setstatus'>
-                 <StatusPopUp/>  
-                
-
-            </div>
-            </div> 
-        <ChatListContainer onEdit={handleEdit} messages={messages.length === 0? props.messages : messages} />
-        <div> 
-            
-        <div className='chatInputDiv'>
-
-
-            <ChatSend onClick={onMessageSend}/>
-            <input type="text" className="chatInputField" value={message} placeholder='Type something here ...' onChange={handleInputChange} onKeyDown={onkeyPressHandler}/>
-</div>
-           
+  return (
+    <div
+      className="ChatRoom"
+      onContextMenu={(e) => {
+        e.preventDefault();
+        console.log("Right Click");
+      }}
+    >
+      <div className="profileNav">
+        {props.user.profileImg ? (
+          <img
+            alt="user profile"
+            className="chatProfile"
+            src={props.user.profileImg}
+          />
+        ) : (
+          <img
+            alt="user profile"
+            className="chatProfile"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBrq9rrEZy6VUsQmoeIPh6gYzS_2JqKe1i9A&usqp=CAU"
+          />
+        )}
+        <p className="profilename">{props.username}</p>
+        <div class="recentSentAt1">lastseen recently</div>
+        <div className="setstatus">
+          <StatusPopUp />
         </div>
+      </div>
+      <ChatListContainer
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+        messages={messages.length === 0 ? props.messages : messages}
+      />
+      <div>
+        <div className="chatInputDiv">
+          <ChatSend onClick={onMessageSend} />
+          <input
+            type="text"
+            className="chatInputField"
+            value={message}
+            placeholder="Type something here ..."
+            onChange={handleInputChange}
+            onKeyDown={onkeyPressHandler}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
