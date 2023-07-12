98abe9bc469fb35df327d94ee60614121fdf6f37
ademecheklie <ademecheklie78@gmail.com>
2023-07-12 08:45:09 +0300

    date display for same day messages added

--- front/src/components/activeMessageScreen.js
+++ front/src/components/activeMessageScreen.js
@@ -7,21 +7,42 @@ import { formatMessageDate } from "../common/Common";
 export function ChatListContainer({ messages, onEdit , name, onDelete}) {
   const messageDisplayRef = useRef(null);
   const [message, setMessage] = useState([]);
+  const [currentDate, setCurrentDate] = useState(formatMessageDate(new Date()));
 
   const scrollToBottom = () => {
-    messageDisplayRef.current.scrollTop =
-      messageDisplayRef.current.scrollHeight;
+    messageDisplayRef.current.scrollTop = messageDisplayRef.current.scrollHeight;
   };
 
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(formatMessageDate(new Date()));
+    }, 1000 * 60 * 60 * 24); // Update date every day (24 hours)
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   const handleEditClick = (messageID, message) => {
     onEdit(messageID, message);
   };
-  const hendleDeleteClick =(messageID) => {
+
+  const handleDeleteClick = (messageID) => {
     onDelete(messageID);
+  };
+
+  let previousDate = null;
+
+  const formatDisplayDate = (date) => {
+    const formattedDate = formatMessageDate(date);
+    if (formattedDate === currentDate) {
+      return "Today";
+    }
+    return formattedDate;
   };
 
   return (
@@ -29,18 +50,28 @@ export function ChatListContainer({ messages, onEdit , name, onDelete}) {
       {messages.length === 0 ? (
         <div className="emptyError"></div>
       ) : (
-        messages.map((message, index) => (
-          <MessageView 
-          name= {name}
-            key={index}
-            created_at={formatMessageDate(new Date(message.created_at))}
-            messageID={message.id}
-            message={message.text}
-            isSenders={currentUser.userId === message.senderId}
-            onDelete={hendleDeleteClick}
-            onEdit={handleEditClick}
-          />
-        ))
+        messages.map((message, index) => {
+          const messageDate = formatMessageDate(new Date(message.created_at));
+          const showDate = previousDate !== messageDate;
+
+          previousDate = messageDate;
+
+          return (
+            <>
+              {showDate && <div>{formatDisplayDate(new Date(message.created_at))}</div>}
+              <MessageView 
+                name={name}
+                key={index}
+                created_at={formatDisplayDate(new Date(message.created_at))}
+                messageID={message.id}
+                message={message.text}
+                isSenders={currentUser.userId === message.senderId}
+                onDelete={handleDeleteClick}
+                onEdit={handleEditClick}
+              />
+            </>
+          );
+        })
       )}
     </div>
   );
